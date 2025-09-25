f206c85dbdebc110fb56a0bc7d40c2ad4dfb0dd2
Alejandra <ale.szuster@gmail.com>
2025-09-25 14:35:08 -0300

    turbopack fix

--- src/app/admin/[[...tool]]/page.jsx
+++ src/app/admin/[[...tool]]/page.jsx
@@ -5,7 +5,8 @@
 import { NextStudio } from 'next-sanity/studio'
 import config from '../../../../sanity.config'
 
-export const dynamic = 'force-static'
+// Remove force-static as it can cause issues with Sanity Studio
+export const dynamic = 'force-dynamic'
 
 export { metadata, viewport } from 'next-sanity/studio'
 
