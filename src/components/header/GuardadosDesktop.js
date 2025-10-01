7a6580ec8ff0f0d4c7c0bf9cb9ac7231525cdf93
Alejandra <ale.szuster@gmail.com>
2025-10-01 09:49:31 -0300

    adding space to names

--- src/components/header/GuardadosDesktop.js
+++ src/components/header/GuardadosDesktop.js
@@ -52,7 +52,7 @@ const GuardadosDesktop = ({ isOpen, onClose }) => {
                     key={`${columnIndex}-${index}`}
                     className="flex justify-start items-center gap-[16px] h-[12px] my-[12px]"
                   >
-                    <div className="flex justify-between items-center gap-[16px] border-r border-grey-10 pr-[8px] h-[12px] lg:w-[105px]">
+                    <div className="flex justify-between items-center gap-[16px] border-r border-grey-10 pr-[8px] h-[12px] lg:w-[110px]">
                       <p>{model.name} {model.lastName?.charAt(0)}.</p>
                       <div
                         className="cursor-pointer hover:opacity-70 transition-opacity"
